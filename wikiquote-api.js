73cdcaa824284f2cb4e661996535c8db467f81f7
Nate Tyler <nathan.tyler@live.com>
2013-07-03 23:55:51 -0400

    Initial commit of api file

---
+++ wikiquote-api.js
@@ -0,0 +1,169 @@
+var WikiquoteApi = (function() {
+
+  var wqa = {};
+
+  var QUERY_URL = "http://en.wikiquote.org/w/api.php?format=json&action=query&redirects&titles=";
+  var PARSE_SECTIONS_URL = "http://en.wikiquote.org/w/api.php?format=json&action=parse&prop=sections&pageid=";
+  var PARSE_TEXT_URL = "http://en.wikiquote.org/w/api.php?format=json&action=parse&noimages&pageid=";
+  var JSONP_SUFFIX = "&callback=?";
+
+  /**
+   * Query based on "titles" parameter and return page id.
+   * If multiple page ids are returned, choose the first one.
+   * Query includes "redirects" option to automatically traverse redirects.
+   * All words will be capitalized as this generally yields more consistent results.
+   */
+  wqa.queryTitles = function(titles, success, error) {
+    $.ajax({
+      url: QUERY_URL + wqa.capitalizeString(titles) + JSONP_SUFFIX,
+      dataType: "jsonp",
+
+      success: function(result, status) {
+        var pages = result.query.pages;
+        var pageId = -1;
+        for(var p in pages) {
+          var page = pages[p];
+          // api can return invalid recrods, these are marked as "missing"
+          if(!("missing" in page)) {
+            pageId = page.pageid;
+            break;
+          }
+        }
+        if(pageId > 0) {
+          success(pageId);
+        } else {
+          error("No results");
+        }
+      },
+
+      error: function(xhr, result, status){
+        error("Error processing your query");
+      }
+    });
+  };
+
+  /**
+   * Get the sections for a given page.
+   * This makes parsing for quotes more manageable.
+   * Returns an array of all "1.x" sections as these usually contain the quotes.
+   * If no 1.x sections exists, returns section 1.
+   */
+  wqa.getSectionsForPage = function(pageId, success, error) {
+    $.ajax({
+      url: PARSE_SECTIONS_URL + pageId + JSONP_SUFFIX,
+      dataType: "jsonp",
+
+      success: function(result, status){
+        var sectionArray = [];
+        var sections = result.parse.sections;
+        for(var s in sections) {
+          var splitNum = sections[s].number.split('.');
+          if(splitNum.length > 1 && splitNum[0] == "1") {
+            sectionArray.push(sections[s].index);
+          }
+        }
+        // Use section 1 if there are no "1.x" sections
+        if(sectionArray.length == 0) {
+          sectionArray.push("1");
+        }
+        success(sectionArray);
+      },
+      error: function(xhr, result, status){
+        error("Error getting sections");
+      }
+    });
+  };
+
+  /**
+   * Get all quotes for a given section.  Most sections will be of the format:
+   * <h3> title </h3>
+   * <ul>
+   *   <li> 
+   *     Quote text
+   *     <ul>
+   *       <li> additional info on the quote </li>
+   *     </ul>
+   *   </li>
+   * <ul>
+   * <ul> next quote etc... </ul>
+   *
+   * The quote may or may not contain sections inside <b /> tags.
+   *
+   * For quotes with bold sections, only the bold part is returned for brevity
+   * (usually the bold part is more well known).
+   * Otherwise the entire text is returned.
+   */
+  wqa.getQuotesForSection = function(pageId, sectionIndex, success, error) {
+    $.ajax({
+      url: PARSE_TEXT_URL + pageId + "&section=" + sectionIndex + JSONP_SUFFIX,
+      dataType: "jsonp",
+
+      success: function(result, status){
+        var quotes = result.parse.text["*"];
+        var quoteArray = []
+
+        // Find top level <li> only
+        var $lis = $(quotes).find('li:not(li li)');
+        $lis.each(function() {
+          // Remove all children that aren't <b>
+          $(this).children().remove(':not(b)');
+          var $bolds = $(this).find('b');
+
+          // If the section has bold text, use it.  Otherwise pull the plain text.
+          if($bolds.length > 0) {
+            quoteArray.push($bolds.html());
+          } else {
+            quoteArray.push($(this).html());
+          }
+        });
+        success(quoteArray);
+      },
+      error: function(xhr, result, status){
+        error("Error getting quotes");
+      }
+    });
+  };
+
+  /**
+   * Get a random quote for the given title search.
+   * This function searches for a page id for the given title, chooses a random
+   * section from the list of sections for the page, and then chooses a random
+   * quote from that section.
+   */
+  wqa.getRandomQuote = function(titles, success, error) {
+
+    var errorFunction = function(msg) {
+      error(msg);
+    };
+
+    var chooseQuote = function(quoteArray) {
+      var randomNum = Math.floor(Math.random()*quoteArray.length);
+      success(quoteArray[randomNum]);
+    };
+
+    var getQuotes = function(pageId, sections) {
+      var randomNum = Math.floor(Math.random()*sections.length);
+      wqa.getQuotesForSection(pageId, sections[randomNum], chooseQuote, errorFunction);
+    };
+
+    var getSections = function(pageId) {
+      wqa.getSectionsForPage(pageId, function(sections) { getQuotes(pageId, sections); }, errorFunction);
+    };
+
+    wqa.queryTitles(titles, getSections, errorFunction);
+  };
+
+  /**
+   * Capitalize the first letter of each word
+   */
+  wqa.capitalizeString = function(input) {
+    var inputArray = input.split(' ');
+    var output = [];
+    for(s in inputArray) {
+      output.push(inputArray[s].charAt(0).toUpperCase() + inputArray[s].slice(1));
+    }
+    return output.join(' ');
+  };
+
+  return wqa;
+}());
